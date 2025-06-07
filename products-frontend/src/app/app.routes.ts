145af33616038e1a188fb41dd3fdf780b15630f0
Santiago Nieto <stiago.r89@gmail.com>
2025-06-07 11:29:25 -0500

    Definiendo rutas products y new

--- products-frontend/src/app/app.routes.ts
+++ products-frontend/src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { ProductList } from './products/product-list/product-list';
+import { ProductForm } from './products/product-form/product-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/products',
+        pathMatch:'full'
+    },
+    {
+        path: 'products',
+        component: ProductList
+    },
+    {
+        path: 'products/new',
+        component: ProductForm
+    },
+    {
+        path: '**',
+        redirectTo: 'products'
+    }
+];
--- products-frontend/src/app/app.ts
+++ products-frontend/src/app/app.ts
@@ -1,18 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import { ProductForm } from "./products/product-form/product-form";
-import { ProductList } from "./products/product-list/product-list";
 
 //Decorador- agregar componentes
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, ProductForm, ProductList],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatButtonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'My Products';
-  page = 0;
 }
