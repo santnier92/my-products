9d53442e41213754314184a114f6e89060b0edc8
Santiago Nieto <stiago.r89@gmail.com>
2025-06-07 11:46:30 -0500

    agregando el backend a mi aplicacion

---
+++ products-backend/src/main/java/com/devsenior/santnier/products_backend/ProductsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.santnier.products_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsBackendApplication.class, args);
+	}
+
+}
